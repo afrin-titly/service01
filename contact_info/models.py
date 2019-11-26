bb3414a8877850a5aa358b3f717cc92365d73e34
afrin <afrin.deeba1114@gmail.com>
2019-11-26 20:31:18 +0900

    define model CustomerContactInfo

--- contact_info/models.py
+++ contact_info/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class CustomerContactInfo(models.Model):
+    name = models.CharField(max_length=30)
+    phone_no = models.CharField(max_length=15)
+    operator = models.CharField(max_length=15)
